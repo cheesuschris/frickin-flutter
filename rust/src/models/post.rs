68ebf99b09f16a70ac02b9f43f23e96930eb29d2
wings20055 <wings20055@gmail.com>
2025-06-09 13:29:54 -0400

    fixd syntax error for oen line of code

--- rust/src/models/post.rs
+++ rust/src/models/post.rs
@@ -1,12 +1,22 @@
-use serde::{Deserialize, Serialize};
 use chrono::{DateTime, Utc};
 use flutter_rust_bridge::frb;
+use serde::{Deserialize, Serialize};
 mod models;
 pub use models::*;
 
 impl Post {
-    pub fn new(recipe:String, initRecipeImage: String, ratingcount: i32, avgDiffRating: f32, avgTasteRating: f32, avgCostRating: f32, allImageFollowUpsToCompare: String, timeStamp: DateTime<Utc>, caption: Striing) -> Post{
-        Post{
+    pub fn new(
+        recipe: String,
+        initRecipeImage: String,
+        ratingcount: i32,
+        avgDiffRating: f32,
+        avgTasteRating: f32,
+        avgCostRating: f32,
+        allImageFollowUpsToCompare: String,
+        timeStamp: DateTime<Utc>,
+        caption: Striing,
+    ) -> Post {
+        Post {
             recipe,
             initRecipeImage,
             ratingcount: 1,
@@ -16,31 +26,29 @@ impl Post {
             allImageFollowUpsToCompare,
             timeStamp: Utc::now(),
             caption,
-            score: 0.0001
+            score: 0.0001,
         }
     }
     pub fn setCap(&mut self, cap: String) {
         self.caption = cap;
     }
-    pub fn setRec(&mut self, rec:String) {
+    pub fn setRec(&mut self, rec: String) {
         self.recipe = rec;
     }
     pub fn resetTime(&mut self) {
         self.timeStamp = Utc::now();
     }
-    pub fn setImg(@mut self, url: String) {
+    pub fn setImg(&mut self, url: String) {
         self.initRecipeImage = url;
     }
     fn incRatingCount(&mut self) {
-        self.ratingcount+=1;
+        self.ratingcount += 1;
     }
-    fn avgChange(&mut self, diff:f32, taste: f32, cost: f32) {
-        self.avgDiffRating = (self.avgDiffRating*ratingcount+diff)/(ratingcount+1);
-        self.avgCostRating = (self.avgCostRating*ratingcount+cost)/(ratingcount+1);
-        self.avgTasteRating = (self.avgTasteRating*ratingcount+taste)/(ratingcount+1);
+    fn avgChange(&mut self, diff: f32, taste: f32, cost: f32) {
+        self.avgDiffRating = (self.avgDiffRating * ratingcount + diff) / (ratingcount + 1);
+        self.avgCostRating = (self.avgCostRating * ratingcount + cost) / (ratingcount + 1);
+        self.avgTasteRating = (self.avgTasteRating * ratingcount + taste) / (ratingcount + 1);
         self.incRatingCount();
     }
-    fn scoreCalc() {
-        
-    }
+    fn scoreCalc() {}
 }
