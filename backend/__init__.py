73556219f821115a64df73640d046b6a698a55b5
Aprameya Kannan <aprameyakannan@gmail.com>
2025-09-13 00:23:59 -0400

    added backend

---
+++ backend/__init__.py
@@ -0,0 +1,17 @@
+#external imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@db.egdumlkkfxknccmvgzxo.supabase.co:5432/postgres"
+    db.init_app(app)
+
+    #internal imports (no circular imports)
+    from .users.routes import users
+    from .recipe_posts.routes import recipe_posts
+    app.register_blueprint(users)
+    app.register_blueprint(recipe_posts)
+    
+    return app
